3c1126991f62ba1d7fc97eca636e4bd2a28c9eff
EamonnMR <eamonn.m.r@gmail.com>
2025-02-25 02:16:58 -0500

    Adds (janky broken) lerping

--- Util.gd
+++ Util.gd
@@ -18,3 +18,12 @@ func is_local():
 #func is_headless():
 	## TODO: How to determine this?
 	#return false
+
+func raise_25d(two_d_vec: Vector2) -> Vector3:
+	return Vector3(two_d_vec.x, 0, two_d_vec.y)
+
+func flatten_25d(three_d_vec: Vector3) -> Vector2:
+	return Vector2(three_d_vec.x, three_d_vec.z)
+	
+func flatten_rotation(object: Node3D) -> float:
+	return object.global_transform.basis.get_rotation_quaternion().get_euler().y
